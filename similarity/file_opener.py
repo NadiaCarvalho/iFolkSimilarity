fb86af937e763698b614393f62361d8bb202fdd7
DanielDiogo113 <up201706208@fe.up.pt>
2022-05-05 12:32:03 +0100

    Songs Loaded
    Loading of songs done

--- similarity/file_opener.py
+++ similarity/file_opener.py
@@ -11,7 +11,7 @@ import json
 
 #jsonPath = 'C:/Users/User/Documents/Faculdade/5_ano/2_Semestre/Python_Workstation/jsons/ifolk2604.json'
 #jsonPath = 'C:/Users/User/Documents/Faculdade/5_ano/2_Semestre/Python_Workstation/jsons/ifolk0205.json'
-jsonPath = 'C:/Users/User/Documents/Faculdade/5_ano/2_Semestre/Python_Workstation/jsons/ifolk0405test.json'
+jsonPath = 'C:/Users/User/Documents/Faculdade/5_ano/2_Semestre/Python_Workstation/iFolkSimilarity/jsons/ifolk0505.json'
 
 
 def loadFeatures(filePath):
@@ -23,3 +23,5 @@ def loadFeatures(filePath):
         songFeatures.append(json.loads(line))
     
     return songFeatures
+
+songs = loadFeatures(jsonPath)

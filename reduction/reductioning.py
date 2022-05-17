a30fb47f0d037e07b22f0caed7f6c973fb423b53
DanielDiogo113 <up201706208@fe.up.pt>
2022-05-17 11:36:44 +0100

    Histogram creation
    Pitch Class histogram creation and Tiv Lib inclusion

--- reduction/reductioning.py
+++ reduction/reductioning.py
@@ -5,21 +5,52 @@ Created on Thu May 12 11:03:36 2022
 @author: User
 """
 
+"""
+ws(k)={2, 11, 17, 16, 19, 7}
+"""
+
 from TIVlib import TIV as tiv
-from os import walk
-mei_path = 'C:/Users/User/Documents/Faculdade/5_ano/2_Semestre/Datasets/I-Folk'
+from collections import Counter
+import json
 
-i=0
-f_path = []
-for (dirpath, dirnames, filenames) in walk(mei_path):
-    for f in filenames:
-        print("File %d out of %d" %(i+1, len(filenames)))
-        f_path.append(dirpath + '/' + f)
-        fHpcp = file_to_hpcp(f_path)
-        fileTiv = tiv.from_pcp(fHpcp)
-        print("tiv.mag: " + str(fileTiv.mags()))
-        print("tiv.diatonicity: " + str(fileTiv.diatonicity()))
-        print("tiv.chromacity: " + str(fileTiv.chromaticity()))
-        print("tiv.dissonance: " + str(fileTiv.dissonance()))
-        i+=1
+jsonPath = 'C:/Users/User/Documents/Faculdade/5_ano/2_Semestre/Python_Workstation/iFolkSimilarity/jsons/ifolk0505.json'
 
+def loadFeatures(filePath):
+    songFeatures = []
+    f = open(filePath, "r")
+    fileLines = f.readlines()
+
+    for line in fileLines:
+        songFeatures.append(json.loads(line))
+    
+    return songFeatures
+
+""" MAIN STARTS HERE """
+#             C C+  D D+  E  F F+  G G+  A A+  B
+
+
+notes = {
+    'C': 0,
+    'D': 2,
+    'E': 4,
+    'F': 5,
+    'G': 7,
+    'A': 9,
+    'B': 11,
+    '+': 1,
+    '-': -1
+    }
+
+songs = loadFeatures(jsonPath)
+pitchClasses = {}
+
+for song in songs:
+    auxHist = Counter(song['features']['pitch'])
+    songId = song['name'][67:]
+    pitchClasses[songId] = [0] * 12
+    for pitch in auxHist:
+        index = 0
+        for char in pitch:
+            if char in notes:
+                index += notes[char]
+        pitchClasses[songId][index] += auxHist[pitch]

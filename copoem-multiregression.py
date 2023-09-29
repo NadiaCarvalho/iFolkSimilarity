5a2ef843527bc3459be0f3bc1cd49fba293a3782
Nadia Carvalho <nadiacarvalho118@gmail.com>
2023-09-29 10:48:21 +0200

    'small'

---
+++ copoem-multiregression.py
@@ -0,0 +1,12 @@
+"""
+Estive a pensar melhor no trabalho do Daniel e acho que uma forma de
+1) reduzir tal quantidade de resultados e oferecer um melhor método seria criar pesos para cada dimensão e só apresentar o modelo multimodal.
+
+Para isso teríamos de ter alguma data de “teste” a única que me lembro seria usar o anotador 1 (eu…que excluímos)
+como dados de treino e usar multiple linear regression model para tal. Aqui vai um tutorial muito simples com o
+sckit.learn: https://tung-dn.github.io/prog_ML_reg1.html
+A secção 2 explica exactamente o que fazer.
+
+Antes de correres as visualizações, se puderes antes experimentar isto seria ótimo.
+Julgo que terá melhores resultados e fica um modelo mais interessante.
+"""
